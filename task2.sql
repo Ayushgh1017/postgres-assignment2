495fe65c3e7c5d67941b257df9a5b082ce2d053d
Ayushgh1017 <75933380+Ayushgh1017@users.noreply.github.com>
2023-06-26 09:57:39 +0530

    Update task2.sql

--- task2.sql
+++ task2.sql
@@ -1,1 +1,6 @@
-
+-- task2
+INSERT INTO tournament_entry (memberid, tourid, year)
+VALUES (103, 2, 2022)
+-- select m.memberid,m.firstname,t.year from member m left join tournament_entry t on m.memberid = t.memberid;
+select m.firstname as member_name,t.year,count(*) as numbers_of_tours from member m  left join tournament_entry t on m.memberid = t.memberid 
+group by m.firstname,t.year;
