f4d20a5a06667d3c2072f8ae7a6cc88889144638
Ayushgh1017 <75933380+Ayushgh1017@users.noreply.github.com>
2023-06-26 09:57:06 +0530

    Update task1.sql

--- task1.sql
+++ task1.sql
@@ -1,1 +1,7 @@
+-- task 1
 
+create  or replace view member_view as 
+SELECT m.memberid,m.firstname,m.lastname,c.firstname AS coach,t.teamname,m.joindate,m.membertypeid,m.gender FROM member m
+LEFT JOIN member c ON m.coachid = c.memberid  left join team as t on t.manager=m.memberid;
+
+select * from member_view;
