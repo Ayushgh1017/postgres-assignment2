4c401d95245e308a35a87e5a1d8a2965c22d5fdf
Ayushgh1017 <75933380+Ayushgh1017@users.noreply.github.com>
2023-06-26 09:58:40 +0530

    Update task4.sql

--- task4.sql
+++ task4.sql
@@ -1,1 +1,7 @@
+-- task 4
 
+create  or replace view tournament_view as 
+select tt.memberid,t.tourid,t.name,t.country,tt.year,count(*) as number_of_participants from tournament as t left join tournament_entry as tt on t.tourid=tt.tourid 
+group by tt.memberid,tt.year,t.tourid;
+
+select * from tournament_view
